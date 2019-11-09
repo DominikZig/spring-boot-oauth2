251c5315e6726a89f15675d63e17882406b85d7f
Dominik Zig <dominikzig@hotmail.co.nz>
2019-11-09 16:33:59 +1300

    Expanded OAuth2 Facebook authentication.

--- src/main/java/com/myprojects/springbootoauth2/SpringBootOauth2Application.java
+++ src/main/java/com/myprojects/springbootoauth2/SpringBootOauth2Application.java
@@ -3,13 +3,40 @@ package com.myprojects.springbootoauth2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SpringBootOauth2Application {
+@RestController
+public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootOauth2Application.class, args);
+	@RequestMapping("/user")
+	public Principal user(Principal principal)
+	{
+		//generally not a good idea to return a whole Principal, since it might expose info that the browser client doesn't need to know
+		return principal; //describes the currently authenticated user
 	}
 
+	@Override
+	public void configure(HttpSecurity http) throws Exception
+	{
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+					.antMatchers("/", "/login**", "webjars/**", "/error**")
+					.permitAll()
+				.anyRequest()
+					.authenticated();
+	}
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringBootOauth2Application.class, args);
+	}
 }
