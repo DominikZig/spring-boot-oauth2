a73457f8a19ce58c2c86973ee0c98f0461ca6c74
Dominik Zig <dominikzig@hotmail.co.nz>
2019-11-09 23:38:55 +1300

    Completed manual authentication configuration.

--- src/main/java/com/myprojects/springbootoauth2/SpringBootOauth2Application.java
+++ src/main/java/com/myprojects/springbootoauth2/SpringBootOauth2Application.java
@@ -6,14 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,15 +41,16 @@ public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter
 	}
 
 	@Override
-	public void configure(HttpSecurity http) throws Exception
+	protected void configure(HttpSecurity http) throws Exception
 	{
 		http
 				.antMatcher("/**")
 				.authorizeRequests()
-					.antMatchers("/", "/login**", "webjars/**", "/error**")
+					.antMatchers("/", "/login**", "/webjars/**", "/error**")
 					.permitAll()
 				.anyRequest()
 					.authenticated()
+				.and().exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
 		        .and().logout().logoutSuccessUrl("/").permitAll()
                 .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 				.and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
@@ -79,6 +83,17 @@ public class SpringBootOauth2Application extends WebSecurityConfigurerAdapter
 	public ResourceServerProperties facebookResource()
 	{
 		return new ResourceServerProperties();
+	}
+
+	@Bean
+	public FilterRegistrationBean<OAuth2ClientContextFilter> oAuth2ClientFilterRegistration(OAuth2ClientContextFilter filter)
+	{
+		FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+
+		return registration;
 	}
 
 	public static void main(String[] args)
